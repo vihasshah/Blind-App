11df27140a67af1a98abd168acd7170b77e64625
Vihas Shah <vihasshah@gmail.com>
2017-03-08 13:25:44 +0530

    gestures code update (Working top, bottom, down, up)

--- app/src/main/java/com/blindapp/MainActivity.java
+++ app/src/main/java/com/blindapp/MainActivity.java
@@ -88,12 +88,12 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         try {
-            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH){
-                return false;
-            }
-            if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH){
-                return false;
-            }
+//            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH){
+//                return false;
+//            }
+//            if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH){
+//                return false;
+//            }
             // right to left swipe
             if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
                     && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
